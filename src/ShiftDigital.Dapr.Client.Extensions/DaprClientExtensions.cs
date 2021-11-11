3098e8102149f4d02bbdd72a26d2decf10a4dba2
Harihara Ramanathan <hramanathan@shiftdigital.com>
2021-11-11 17:03:22 -0500

    added hashset

--- src/ShiftDigital.Dapr.Client.Extensions/DaprClientExtensions.cs
+++ src/ShiftDigital.Dapr.Client.Extensions/DaprClientExtensions.cs
@@ -37,5 +37,36 @@ namespace Dapr.Client
             }
             return result;
         }
+
+        public static async Task<bool> TrySaveSetStateAsync<TValue>(this DaprClient daprClient, string storeName, string key, Func<HashSet<TValue>, Task<HashSet<TValue>>> setFactory, short retryAttempts = 1, short retryWaitTimeInSeconds = 5, CancellationToken cancellationToken = default)
+        {
+            var (set, etag) = await daprClient.GetStateAndETagAsync<HashSet<TValue>>(storeName, key, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            set = await setFactory(set).ConfigureAwait(false);
+
+            var result = await daprClient.TrySaveStateAsync(storeName, key, set, etag, cancellationToken: cancellationToken).ConfigureAwait(false);
+            if (!result)
+            {
+                while (!result && retryAttempts > 0)
+                {
+                    if (retryWaitTimeInSeconds > 0)
+                    {
+                        await Task.Delay(retryWaitTimeInSeconds * 1000, cancellationToken).ConfigureAwait(false);
+                    }
+
+                    retryAttempts = (short)(retryAttempts - 1);
+
+                    var (setOnNextAttempt, etagOnNextAttempt) = await daprClient.GetStateAndETagAsync<HashSet<TValue>>(storeName, key, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+                    setOnNextAttempt = await setFactory(setOnNextAttempt).ConfigureAwait(false);
+
+                    result = await daprClient.TrySaveStateAsync(storeName, key, setOnNextAttempt, etagOnNextAttempt, cancellationToken: cancellationToken).ConfigureAwait(false);
+                }
+
+                if (!result)
+                    return result;
+            }
+            return result;
+        }
     }
 }
